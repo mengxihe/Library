e41a865a77924e09382697fc69792ab91d0fe37b
Mengxi He <mengxihex@gmail.com>
2022-07-07 11:45:20 +0200

    updated file structures

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-
-
 console.log('Hello');
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    //the constructor..
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary () {
+    // do stuff here
+}
