510603f1cbcb71459fc93888cb306b0cae1ce525
Mengxi He <mengxihex@gmail.com>
2022-07-18 11:07:46 +0200

    some functions

--- script.js
+++ script.js
@@ -17,26 +17,56 @@ class Book {
 }
 
 
-const addBookBtn = document.getElementById('add-book-btn')
-
+const addBookBtn = document.getElementById('add-book-btn');
+const submit = document.getElementById('submit');
+const booksGrid = document.getElementById('booksGrid');
 
 const getBookInput = () =>{
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const read = document.getElementById('read').value;
-    return new Book(title, author, pages, read)
+    return new Book(title, author, pages, read);
 }
 
 const addBook = (e) => {
-    e.preventDefault()
-    const newBook = getBookInput()
-    myLibrary.push(newBook)
+    e.preventDefault();
+    const newBook = getBookInput();
+    myLibrary.push(newBook);
+    updateBooksGrid(myLibrary);
 }
 
+const updateBooksGrid = () => {
+    resetBooksGrid();
+    for (let book of myLibrary) {
+        createBookCard(book)
+    }
+}
+
+const resetBooksGrid = () => {
+    booksGrid.innerHTML = '';
+}
+
+const createBookCard = (book) => {
+    const bookCard = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const btnGroup = document.createElement('div');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    title.textContent = `'${book.title}'`;
+    author.textContent =`'${book.author}'`;
+    pages.textContent = `'${book.author}'`;
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    booksGrid.appendChild(bookCard)
+
+}
+
+submit.onclick = addBook
 
 
-const submit = document.getElementById('submit');
-submit.onclick = addBook;
-
-console.log(myLibrary)
